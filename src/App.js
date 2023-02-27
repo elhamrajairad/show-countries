c74c1519e2c5021e0bb03208955e83c575b536eb
elham rajaee <elham.rajairad@gmail.com>
2023-02-27 19:48:54 +0330

    change file index

--- src/App.js
+++ src/App.js
@@ -12,10 +12,7 @@ const App = () => {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route
-            path="https://elhamrajairad.github.io/show-countries/"
-            element={<Home />}
-          />
+          <Route path="/" element={<Home />} />
           <Route path="/products" element={<Cards />} />
           <Route path="/products/:slug" element={<CountryDetail />} />
           <Route path="*" element={<PageNotFound />} />
